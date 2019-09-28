09aad2fd417fd186c1f9432d5679efc9e1d2dac3
Volodymyr Tytarenko <bovatitar@gmail.com>
2019-09-28 13:51:12 +0200

    * first db migration

---
+++ database/migrations/2019_09_27_211136_create_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('login', 255);
+            $table->string('password', 255);
+            $table->string('email', 255);
+            $table->string('token', 255);
+            $table->dateTime('last_login');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
---
+++ database/migrations/2019_09_27_212112_create_menu_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_item', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->integer('menu_id');
+            $table->string('alias',255);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('menu_id')->references('id')->on('menu');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_item');
+    }
+}
